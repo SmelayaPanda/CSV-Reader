bf96357abd83df50baf6daf74cdb2149b5f8bc7a
SmelayaPanda <smelayapanda@mail.ru>
2016-05-20 13:46:00 +0600

    redacted TxtFileReader

--- src/reader/TxtFileReader.java
+++ src/reader/TxtFileReader.java
@@ -2,54 +2,44 @@ package reader;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
 
 public class TxtFileReader {
-    public static Map readFile(String fileName, Map<String,Integer> map) {
+    public static Map readFile(String fileName, Map<String, Integer> map) {
 
         BufferedReader in = null;
         try {
-            in = new BufferedReader(new java.io.FileReader(fileName));
-        } catch (FileNotFoundException e) {
-            System.err.println("File not found");
-        }
-        StringBuilder sb = new StringBuilder();
-
-        List<String> list = new ArrayList<>();
-
-
-        try {
+            in = new BufferedReader(new FileReader(fileName));
+            StringBuilder sb = new StringBuilder();
             try {
-                int s;
-                int i = 0;
-                while (((s = in.read()) != -1)) {
-
-                    if (Character.isLetterOrDigit(s)) {
-                        sb.append((char) s);
-                    } else {
-                        String slovo = String.valueOf(sb);
-                        sb.delete(0, sb.length());
-                        if (map.get(slovo) == null) {
-                            map.put(slovo, 1);
+                try {
+                    int s;
+                    while (((s = in.read()) != -1)) {
+                        if (Character.isLetterOrDigit(s)) {
+                            sb.append((char) s);
                         } else {
-                            int newKey = map.get(slovo) + 1;
-                            map.remove(slovo);
-                            map.put(slovo, newKey);
+                            String slovo = String.valueOf(sb);
                             sb.delete(0, sb.length());
-                        }
-                        if ((char) s == '\n') {
-                            sb.delete(0, sb.length());
+                            if ((map.get(slovo) == null) && !slovo.equals("the") && !slovo.equals("")) {
+                                map.put(slovo, 1);
+                            } else if (map.get(slovo) != null) {
+                                int newKey = map.get(slovo) + 1;
+                                map.remove(slovo);
+                                map.put(slovo, newKey);
+                            }
                         }
                     }
+                } finally {
+                    in.close();
                 }
-            } finally {
-                in.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found");
         }
         return map;
     }
 }
-
